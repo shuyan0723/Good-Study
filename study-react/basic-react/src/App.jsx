c4e89fb3cb0e303c6b68b6381020734296bd9230
shuyan0723 <2737559680@qq.com>
2025-07-19 20:06:02 +0800

    jsx 复杂渲染

--- study-react/basic-react/src/App.jsx
+++ study-react/basic-react/src/App.jsx
@@ -66,39 +66,64 @@ import './App.css'
 // }
 // useState实现一个计数器按钮
 
+// function App() {
+//   // 1.调用useState添加一个状态变量
+//   // count 状态变量
+//   // setCount 更新状态变量的函数 修改状态变量的方法
+//   // const [count, setCount] = useState(0)
+//   let [count, setCount] = useState(0)
+
+//   // 2.点击事件回调
+//   const handleClick = () => {
+//     // 作用:1.用传入的新值修改count
+//     // 2.重新使用新的count渲染UI
+//     setCount(count + 1)
+//     // count++
+//     // console.log(count)
+//   }
+//   const [form, setForm] = useState({ name: '张三' }) // 不能在函数组件中调用useState
+//   const changeForm = () => {
+//     // 错误写法:直接修改form对象的属性值
+//     // 这样做会导致React无法检测到状态的变化，从而不会触发
+//     //form.name = '李四' // 直接修改form对象的属性值
+//     // 正确写法:使用setForm函数来更新状态
+//     // setForm({ name: '李四' }) // 传入一个新的对象
+//     setForm({
+//       ...form,
+//       name: '李四' // 使用展开运算符保留其他属性
+//     })
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={handleClick}>Count:{count}</button>
+//       <button onClick={changeForm}>修改form{form.name}</button>
+
+//     </div>
+//   )
+// }
 function App() {
-  // 1.调用useState添加一个状态变量
-  // count 状态变量
-  // setCount 更新状态变量的函数 修改状态变量的方法
-  // const [count, setCount] = useState(0)
-  let [count, setCount] = useState(0)
-
-  // 2.点击事件回调
-  const handleClick = () => {
-    // 作用:1.用传入的新值修改count
-    // 2.重新使用新的count渲染UI
-    setCount(count + 1)
-    // count++
-    // console.log(count)
-  }
-  const [form, setForm] = useState({ name: '张三' }) // 不能在函数组件中调用useState
-  const changeForm = () => {
-    // 错误写法:直接修改form对象的属性值
-    // 这样做会导致React无法检测到状态的变化，从而不会触发
-    //form.name = '李四' // 直接修改form对象的属性值
-    // 正确写法:使用setForm函数来更新状态
-    // setForm({ name: '李四' }) // 传入一个新的对象
-    setForm({
-      ...form,
-      name: '李四' // 使用展开运算符保留其他属性
-    })
-  }
-
+  const songs = [
+    { id: 1, name: "演员" },
+    { id: 2, name: "江南" },
+    { id: 3, name: "曹操" },
+  ]
+  const name = '超人强'
+  const flag = true
   return (
-    <div>
-      <button onClick={handleClick}>Count:{count}</button>
-      <button onClick={changeForm}>修改form{form.name}</button>
-
+    <div className='App'>
+      {/* <ul>
+        {
+          songs.map((item) => {
+            // return <li>{item.name}</li>
+            return <li>{item.id}
+              {item.name}</li>
+          })
+        }
+      </ul> */}
+      <h1>猪猪侠{name}</h1>
+      <h3>{flag ? 'react好' : 'react坏'}</h3>
+      <div>{flag ? <span>看</span> : null}</div>
     </div>
   )
 }
---
+++ study-react/readme.md
@@ -0,0 +1,11 @@
+XML 早期用于存储和传输数据
+<student>
+<name>Tom</name>
+<age>19<age>
+</student>
+
+JSON 
+"{"name":"Tom","age":19}"
+
+JSON:parse
+     stringify
